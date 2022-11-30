a07208b1e8035d79a2374c6f2b4a460f4d4cf40d
Daniel <danielipkind@gmail.com>
2022-11-30 09:26:10 +0200

    Add initial functions structure

--- script.js
+++ script.js
@@ -3,16 +3,71 @@ const btnOperators = document.querySelectorAll('.operator');
 const btnEqual = document.querySelectorAll('.equal');
 const btnDot = document.querySelectorAll('.dot');
 const btnClear = document.querySelectorAll('.clear');
+const display = document.querySelector('.display');
 
-btnNumbers.forEach(button => {button.addEventListener('click', numberCheck)});
-btnOperators.forEach(button => {button.addEventListener('click', numberCheck)});
-btnEqual.forEach(button => {button.addEventListener('click', numberCheck)});
-btnDot.forEach(button => {button.addEventListener('click', numberCheck)});
-btnClear.forEach(button => {button.addEventListener('click', numberCheck)});
+let displayValue = 0;
+let value1;
+let value2;
+let operatorValue;
 
-function numberCheck() {
-  alert("Hello");
+display.textContent = displayValue; 
+
+btnNumbers.forEach(button => {button.addEventListener('click', number)});
+btnOperators.forEach(button => {button.addEventListener('click', operator)});
+btnEqual.forEach(button => {button.addEventListener('click', equals)});
+btnDot.forEach(button => {button.addEventListener('click', dot)});
+btnClear.forEach(button => {button.addEventListener('click', clear)});
+
+function number(event) {
+  button = event.target;
+  return (displayValue == 0 && button.value == 0) ? false : updateDisplay(button.value);
 }
+
+function updateDisplay(btnValue) {
+  displayValue === 0 ? (displayValue = btnValue) : (displayValue = displayValue + btnValue);
+  return display.textContent = displayValue; 
+}
+
+function resetDisplay() {
+  displayValue = undefined;
+}
+
+function operator(event) {
+  button = event.target;
+  operatorValue = button.value;
+  if (value1 === undefined) {
+    value1 = displayValue;
+    resetDisplay();
+  } else {
+    value2 = resetDisplay;
+    equals(operatorValue);
+  }
+}
+
+function equals(operatorValue) {
+  if (operatorValue === "/" && value2 === 0) {
+    clear();
+  } else {
+    if (operatorValue === "+") {
+      add();  
+    } else if { 
+    } else if {
+    } else if {
+    } else if {
+  }
+  
+}
+
+
+function dot() {
+  null
+}
+
+function clear() {
+  null
+}
+
+
 
 
 
